77861b31e2a744cf6fd8ff83e6054fcc6cdb91bf
LucasDCoelho <darkalp99@gmail.com>
2024-01-24 08:29:19 -0300

    new: start new application using Java with spring

---
+++ src/main/java/com/example/crudgames/CrudGamesApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudGamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudGamesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crudgames/CrudGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudGamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
